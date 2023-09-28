1716b6c6e63c8c2acf38b7ca6022dfbc89d7acb9
root <root@G-V-Utong>
2023-09-28 23:28:20 +0100

    Task 5 update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const roomSizes = [19, 20, 34];
   const classrooms = roomSizes.map((size) => new ClassRoom(size));
-  const formattedClassrooms = classrooms.map((room) => (
-    `ClassRoom { _maxStudentsSize: ${room._maxStudentsSize} }`));
-  return formattedClassrooms;
+  return classrooms;
 }
+
+console.log(initializeRooms());
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+      this._sqft = sqft;
+    }
+
+  get sqft() {
+    return this._sqft
+  }
+
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
+
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+    new TestBuilding(200)
+}
+catch(err) {
+    console.log(err);
+}
