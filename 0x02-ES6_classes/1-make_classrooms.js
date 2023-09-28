cee28d95ba7805b2915d499d67099fb936314866
root <root@G-V-Utong>
2023-09-28 19:21:18 +0100

    Task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,20 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-//   const smallSize = new ClassRoom(19);
-//   const midSize = new ClassRoom(20);
-//   const largeSize = new ClassRoom(34);
   const roomSizes = [19, 20, 34];
   const classrooms = roomSizes.map((size) => new ClassRoom(size));
   const formattedClassrooms = classrooms.map((room) => (
     `ClassRoom { _maxStudentsSize: ${room._maxStudentsSize} }`));
   return formattedClassrooms;
-
-//   return [
-//     `ClassRoom { _maxStudentsSize: ${smallSize._maxStudentsSize} },
-//   ClassRoom { _maxStudentsSize: ${midSize._maxStudentsSize} },
-//   ClassRoom { _maxStudentsSize: ${largeSize._maxStudentsSize} }`
-//    ]
 }
-
-console.log(initializeRooms());
